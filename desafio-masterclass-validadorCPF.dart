2cb6feff253f293b3ec023e13e26d1a9510c23e7
Willian Oliveira <45185265+Willian0liveira@users.noreply.github.com>
2023-03-09 02:13:36 -0300

    Update desafio-masterclass-validadorCPF.dart

--- desafio-masterclass-validadorCPF.dart
+++ desafio-masterclass-validadorCPF.dart
@@ -1,5 +1,5 @@
 void main() {
-  ValidCpf validCpf = ValidCpf(cpf: "DIGITE AQUI O NÚMERO DO CPF SEM PONTOS E TRAÇOS");
+  ValidCpf validCpf = ValidCpf(cpf: "DIGITE AQUI O NÚMERO DO CPF SEM PONTOS E TRAÇO");
   validCpf.validSucessfull();
 }
 
